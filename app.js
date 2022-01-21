691b5ec7ab9ac3da7cbe317f5960e498c37122a1
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2022-01-21 15:43:20 -0500

    seeded the database and synced

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const main = require("./views/main.js");
+const db = require('./models');
+const { Sequelize, DataTypes } = require("sequelize");
+// const Sequelize = require('Sequelize')
+
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
+
 const PORT = 3000;
 
 app.use(express.static(__dirname + "/public"));
@@ -13,6 +24,18 @@ app.get("/", (req, res) => {
     res.send(main());
 });
 
-app.listen(PORT, () => {
-    console.log("Post is listening.");
-});
+
+
+const myAsyncFunction = async() => {
+    await db.sync({force: true})
+
+    app.listen(PORT, () => {
+        console.log("Post is listening.");
+    });
+    
+} 
+
+
+myAsyncFunction()
+
+
